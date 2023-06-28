4ac952756dec0d5b7b9b537169596ed546dab1e5
unknown <bip677@gmail.com>
2023-06-28 16:50:33 +0530

    new changes

---
+++ src/test/java/syne/API/Test1.java
@@ -0,0 +1,19 @@
+package syne.API;
+
+public class Test1 {
+
+	
+	int add(int a, int b)
+	{
+		int c=a+b;
+		return c;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+	}
+
+}
